addb2a179d4ce19080956e51d57a1f2f611b31cc
Rahul Biswas <kumarbiswasrahul4@gmail.com>
2017-09-14 19:59:14 +0530

    React Project
    --Bootstrap Component
    --Github Profile Viwer

---
+++ Basic Bootstrap Coponent/js/reactcus.js
@@ -0,0 +1,16 @@
+var Navbar= React.createClass({
+	render:function () {
+		return (
+			<div>
+				<h1>NAVBAR</h1>
+			</div>
+
+			)
+	}
+});
+
+ReactDOM.render(
+		<Navbar />,
+		document.getElementById('app')
+
+	)
